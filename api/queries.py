7f10e71c7fffa95f449ecaaf501edbd5b5e8bdff
hicham ajarif <hicham@advoncommerce.com>
2023-04-07 16:46:41 -0400

    Add mutation crud

--- api/queries.py
+++ api/queries.py
@@ -1,5 +1,7 @@
 from .models import Post
+from api import db
 from ariadne import convert_kwargs_to_snake_case
+from datetime import date
 
 def listPosts_resolver(obj, info):
     try: 
@@ -28,5 +30,64 @@ def getPost_resolver(obj, info, id):
         payload = {
             "success": False,
             "errors": ["Post item matching {id} not found !"]
+        }
+    return payload
+
+@convert_kwargs_to_snake_case
+def createPost_resolver(obj, info, title, description):
+    try:
+        today = date.today()
+        post = Post(
+            title=title, 
+            description=description, 
+            created_at=today.strftime("%b-%d-%Y")
+        )
+        db.session.add(post)
+        db.session.commit()
+        payload = {
+            "success":True,
+            "post": post.to_dict()
+        }
+    except ValueError:
+        payload = {
+            "success": False,
+            "errors": ["Incorrect data provided"]
+        }
+    return payload
+
+@convert_kwargs_to_snake_case
+def updatePost_resolver(obj, info, id, title, description):
+    try:
+        post = Post.query.get(id)
+        if post:
+            post.title = title
+            post.description = description
+        db.session.add(post)
+        db.session.commit()
+        payload = {
+            "success": True,
+            "post": post.to_dict()
+        }
+    except AttributeError:  # todo not found
+        payload = {
+            "success": False,
+            "errors": ["item matching id {id} not found"]
+        }
+    return payload
+
+@convert_kwargs_to_snake_case
+def deletePost_resolver(obj, info, id):
+    try:
+        post = Post.query.get(id)
+        db.session.delete(post)
+        db.session.commit()
+        payload = {
+            "success": True,
+            "post": post.to_dict()
+        }
+    except AttributeError:  # todo not found
+        payload = {
+            "success": False,
+            "errors": ["item matching id {id} not found"]
         }
     return payload
